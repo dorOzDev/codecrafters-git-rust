76aced620946430a399f49d92dd209db087d8142
dorOzDev <dorzry@gmail.com>
2025-06-17 15:40:46 +0300

    add missing constant

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,6 @@ pub const GIT_DIR: &str = ".git";
 pub const GIT_OBJECTS_DIR: &str = ".git/objects";
 pub const GIT_REF_DIR: &str = ".git/refs";
 pub const GIT_HEAD_PATH: &str = ".git/HEAD";
+pub const INDEX_PATH: &str = ".git/index";
 pub const MAX_INDEX_PATH_LEN: usize = u16::MAX as usize;
 
