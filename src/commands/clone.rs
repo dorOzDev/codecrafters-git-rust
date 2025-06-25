e07ad3ae945b7e086a38c12cafd05552c2204143
dorOzDev <dorzry@gmail.com>
2025-06-25 16:58:45 +0300

    add a basic fetch and parser of refs

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -1,11 +1,15 @@
-use std::{io::{self, Error}, path::PathBuf};
+use std::{io::{self, Cursor, Error}, path::PathBuf};
 
+use git_packetline::{PacketLineRef, StreamingPeekableIter};
+use reqwest::{blocking::Client, header::USER_AGENT};
 
 
 pub fn run(args: &[String]) -> io::Result<()> {
     let clone_args = parse_args(&args)?;
-
-    println!("Running git-clone with args: {:?}", args);
+    let refs_bytes = fetch_refs(&clone_args.url)?;
+    println!("refs bytes len:{}", refs_bytes.len());
+    parse_with_git_packetline(&refs_bytes)?;
+    println!("Running git-clone with args: {},{}", clone_args.url, clone_args.target_dir.display());
     Ok(())
 }
 
@@ -30,6 +34,64 @@ fn parse_args(args: &[String]) -> io::Result<CloneArgs> {
             .unwrap_or("repo");
         PathBuf::from(name)
     };
-
     Ok(CloneArgs { url, target_dir })
+}
+
+fn fetch_refs(url: &str) -> Result<Vec<u8>, std::io::Error> {
+    let service_url = format!("{}/info/refs?service=git-upload-pack", url.trim_end_matches('/'));
+
+    let client = Client::new();
+    let response = client
+        .get(&service_url)
+        .header(USER_AGENT, "git/2.42.0")
+        .send()
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+    if !response.status().is_success() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Request failed with status {}", response.status()),
+        ));
+    }
+
+    let bytes = response
+        .bytes()
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+    Ok(bytes.to_vec())
+}
+
+fn parse_with_git_packetline(bytes: &[u8]) -> std::io::Result<()> {
+    let reader = Cursor::new(bytes);
+    let mut packets = StreamingPeekableIter::new(reader, &[]); // no stopping condition
+
+        while let Some(result) = packets.read_line() {
+            match result {
+                Ok(Ok(packet)) => match packet {
+                    PacketLineRef::Flush => println!("<flush>"),
+                    PacketLineRef::Delimiter => println!("<delimiter>"),
+                    PacketLineRef::ResponseEnd => {
+                        println!("<response-end>");
+                        break;
+                    }
+                    PacketLineRef::Data(data) => {
+                        match std::str::from_utf8(data) {
+                            Ok(s) => println!("DATA: {s}"),
+                            Err(_) => println!("(binary data) {:?}", data),
+                        }
+                    }
+                },
+                Ok(Err(decode_err)) => {
+                    eprintln!("Decode error: {decode_err}");
+                    break;
+                }
+                Err(io_err) => {
+                    eprintln!("I/O error: {io_err}");
+                    return Err(io_err);
+                }
+            }
+    }
+
+    Ok(())
+
 }
