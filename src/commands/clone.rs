097d03b898300291dfdbeecbd18f936735098bfc
dorOzDev <dorzry@gmail.com>
2025-06-29 16:16:46 +0300

    reorder debug prints

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -4,10 +4,9 @@ use crate::clone::{args::parse_args, http::fetch_refs, refs::parse_ref_advertise
 
 pub fn run(args: &[String]) -> io::Result<()> {
     let clone_args = parse_args(&args)?;
+    println!("Running git-clone with args: {},{}", clone_args.url, clone_args.target_dir.display());
     let refs_bytes = fetch_refs(&clone_args.url)?;
-    println!("refs bytes len:{}", refs_bytes.len());
     let refs = parse_ref_advertisement(&refs_bytes)?;
     refs.print_debug();
-    println!("Running git-clone with args: {},{}", clone_args.url, clone_args.target_dir.display());
     Ok(())
 }
