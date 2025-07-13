ee0fedab96f52eb7ebb6b1171bbada5dde4f20c8
dorOzDev <44348538+dorOzDev@users.noreply.github.com>
2025-07-13 14:39:05 +0300

    add creating git dir upon succuss negogiate call. separate create git dir logic to a dedeicated function and set the new dir as hidden on windows machine

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -1,14 +1,44 @@
-use std::io;
-use crate::clone::{args::parse_args, packet_line::pkt_negotiator::run_upload_pck, refs::parse_ref_advertisement, transport::http::fetch_refs};
+use std::{fs, io::{self}, path::Path};
+
+use crate::{clone::{args::parse_args, packet_line::{pkt_negotiator::negogiate_want}, refs::parse_ref_advertisement, transport::http::fetch_refs}, commands::init::init_git_dir, hash::{GitHash, HASH_HEX_LENGTH}};
 
 
 pub fn run(args: &[String]) -> io::Result<()> { 
     let clone_args = parse_args(&args)?;
+    validate_target_dir_empty_or_missing(&clone_args.target_dir)?;
     println!("Running git-clone with args: {},{}", clone_args.url, clone_args.target_dir.display());
     let refs_bytes = fetch_refs(&clone_args.url)?;
     let refs = parse_ref_advertisement(&refs_bytes)?;
+    let res = negogiate_want(&refs, &clone_args.url)?;
+    if !clone_args.target_dir.exists() {
+        fs::create_dir_all(&clone_args.target_dir)?;
+    }
+    init_git_dir(&clone_args.target_dir)?;
+    Ok(())
+}
 
-    run_upload_pck(&refs, &clone_args.url)?;
+pub fn validate_target_dir_empty_or_missing(target: &Path) -> io::Result<()> {
+    if target.exists() {
+        if !target.is_dir() {
+            return Err(io::Error::new(
+                io::ErrorKind::AlreadyExists,
+                format!(
+                    "destination path '{}' exists and is not a directory",
+                    target.display()
+                ),
+            ));
+        }
+
+        if fs::read_dir(target)?.next().is_some() {
+            return Err(io::Error::new(
+                io::ErrorKind::AlreadyExists,
+                format!(
+                    "destination path '{}' already exists and is not an empty directory",
+                    target.display()
+                ),
+            ));
+        }
+    }
 
     Ok(())
 }
--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,12 +1,43 @@
 use std::fs;
 use std::io;
+use std::path::Path;
 use crate::constants::*;
 
 pub fn run() -> io::Result<()> {
-    fs::create_dir_all(GIT_DIR)?;
-    fs::create_dir_all(GIT_OBJECTS_DIR)?;
-    fs::create_dir_all(GIT_REF_DIR)?;
-    fs::write(GIT_HEAD_PATH, "ref: refs/heads/main\n")?;
+    init_git_dir(std::env::current_dir()?.as_path())?;
     println!("Initialized git directory");
+    Ok(())
+}
+
+pub fn init_git_dir(root: &Path) -> io::Result<()> {
+    let git_dir = root.join(GIT_DIR);
+    let objects_dir = root.join(GIT_OBJECTS_DIR);
+    let refs_dir = root.join(GIT_REF_DIR);
+    let head_path = root.join(GIT_HEAD_PATH);
+
+    fs::create_dir_all(&git_dir)?;
+    fs::create_dir_all(&objects_dir)?;
+    fs::create_dir_all(&refs_dir)?;
+    fs::write(&head_path, "ref: refs/heads/main\n")?;
+
+    #[cfg(windows)]
+    {
+        use std::ffi::OsStr;
+        use std::iter::once;
+        use std::os::windows::ffi::OsStrExt;
+        use winapi::um::fileapi::SetFileAttributesW;
+        use winapi::um::winnt::FILE_ATTRIBUTE_HIDDEN;
+
+        let wide: Vec<u16> = OsStr::new(git_dir.as_os_str())
+            .encode_wide()
+            .chain(once(0))
+            .collect();
+
+        unsafe {
+            if SetFileAttributesW(wide.as_ptr(), FILE_ATTRIBUTE_HIDDEN) == 0 {
+                return Err(io::Error::last_os_error());
+            }
+        }
+    }
     Ok(())
 }
